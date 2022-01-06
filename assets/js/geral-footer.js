f741eeb5c38da7393726a7a06e771adc0ea1c1c5
becahp <bcasamo@gmail.com>
2022-01-06 19:39:37 -0300

    Ajuste de js para esconder comentários disqus

--- assets/js/geral-footer.js
+++ assets/js/geral-footer.js
@@ -30,3 +30,19 @@ window.onload = function() {
         elementBody.style.fontSize = fontSize + '%';
     });
 }
+
+/* Olha esse site depois: https://stackoverflow.com/questions/49131980/how-do-i-disable-or-hide-the-unwanted-disqus-ads-on-my-website */
+jQuery(document).ready(function($) {
+
+    const disqus = $('#disqus_thread');
+
+    disqus.ready(function() {
+        setTimeout(function() {
+            if (disqus.children().length >= 3) {
+                const comments = disqus.find('iframe:nth-child(2)').detach();
+                disqus.empty().append(comments);
+            }
+        }, 2000);
+    });
+
+});
